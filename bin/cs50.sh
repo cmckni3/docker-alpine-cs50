8915f16e8e48ff9f3e0f1654e94280ae3febdcce
Chris McKnight <cmckni3@gmail.com>
2015-07-24 11:58:36 -0500

    Create shell script for installing cs50 library

---
+++ bin/cs50.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# download cs50 library
+wget http://mirror.cs50.net/library50/c/library50-c-5.zip
+unzip library50-c-5.zip && rm library50-c-5.zip
+
+# build cs50 library
+cd library50-c-5 && gcc -c -ggdb -std=c99 cs50.c -o cs50.o
+ar rcs libcs50.a cs50.o
+chmod 0644 cs50.h libcs50.a
+mkdir -p /usr/local/include
+chmod 0755 /usr/local/include
+mv -f cs50.h /usr/local/include
+mkdir -p /usr/local/lib
+chmod 0755 /usr/local/lib
+mv -f libcs50.a /usr/local/lib
+cd ..
+rm -rf library50-c-5
